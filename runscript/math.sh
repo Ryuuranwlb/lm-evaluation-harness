f9bf9ee8a7270efea87f26e44ffd85e06f9b9f54
huyiwen <1020030101@qq.com>
2024-12-30 02:11:03 +0800

    fix(script): unify command

--- runscript/math.sh
+++ runscript/math.sh
@@ -1,18 +1,20 @@
-MODEL_PATH=${1:-/media/yulan/sft/mix_3M_no_multi-12-21}
+MODEL_PATH=${1:-/home/u20140041/code/HALOs/data/models/yulan_dummy-dpo-para-1/FINAL}
 
-CUDA_VISIBLE_DEVICES=2 lm_eval --model vllm \
+CUDA_VISIBLE_DEVICES=6 lm_eval --model vllm \
     --model_args pretrained=$MODEL_PATH,trust_remote_code=True \
     --tasks gsm8k_cot_qwen \
     --gen_kwargs max_gen_toks=2048 \
     --batch_size 256 \
-    --apply_chat_template 
+    --apply_chat_template \
+    --output_path model_result \
+    --log_samples
 
 
-CUDA_VISIBLE_DEVICES=2 lm_eval --model vllm \
+CUDA_VISIBLE_DEVICES=6 lm_eval --model vllm \
     --model_args pretrained=$MODEL_PATH,trust_remote_code=True,gpu_memory_utilization=0.9 \
     --tasks hendrycks_math_yulan \
     --apply_chat_template \
     --batch_size 512 \
     --gen_kwargs max_gen_toks=2048 \
-    --output_path result \
+    --output_path model_result \
     --log_samples 
